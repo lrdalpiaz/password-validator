8bbf9ffdbf69ba4b3141224f130b37de32c85d48
Leonardo R. Dalpiaz <leonardo_dalpiaz@sicredi.com.br>
2020-04-18 11:25:59 -0300

    Cria estrutura inicial do projeto
    Signed-off-by: Leonardo R. Dalpiaz <leonardo_dalpiaz@sicredi.com.br>

---
+++ src/main/java/com/iti/password/checker/PasswordCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.iti.password.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iti/password/checker/PasswordCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iti.password.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
